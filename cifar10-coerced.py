7aa6ada1de0b415d965fb32c81042cde875e843b
cat-claws <47313357+cat-claws@users.noreply.github.com>
2025-04-30 02:28:44 +0800

    script

--- cifar10-coerced.py
+++ cifar10-coerced.py
@@ -94,7 +94,7 @@ train_transform = torchvision.transforms.Compose([
 test_transform = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor(),
     # torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    # torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 
