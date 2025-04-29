2fcba3af9b9b5c82abdbdabf96309a8207d0c2f2
cat-claws <47313357+cat-claws@users.noreply.github.com>
2025-04-30 02:21:37 +0800

    script

--- v100.sh
+++ v100.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+# #!/bin/bash
 
-./cluster.sh
+# bash cluster.sh
 
-srun --partition=researchlong --gres=gpu:1 python cifar10-coerced.py --dataset 'cifar10-16-huang2021unlearnable' --model resnet20_svhn --training_step classification_step --optimizer SGD --optimizer-lr 0.1 --optimizer-momentum 0.9 --optimizer-weight_decay 0.0005 --scheduler CosineAnnealingLR  --scheduler-T_max 200 --epochs 201
+# srun --partition=researchlong --gres=gpu:1 python cifar10-coerced.py --dataset 'cifar10-16-huang2021unlearnable' --model resnet20_svhn --training_step classification_step --optimizer SGD --optimizer-lr 0.1 --optimizer-momentum 0.9 --optimizer-weight_decay 0.0005 --scheduler CosineAnnealingLR  --scheduler-T_max 200 --epochs 201
 
-# srun --partition=researchlong --gres=gpu:1 python cifar10-coerced.py --dataset 'cifar10-8-huang2021unlearnable' --model resnet18_cifar --training_step attacked_classification_step  --atk PGD --atk-eps 0.03137254901 --atk-alpha 0.00784313725 --atk-steps 10 --optimizer AdamW --scheduler CosineAnnealingLR  --scheduler-T_max 200 --epochs 201
+srun --partition=researchshort --gres=gpu:1 python cifar10-coerced.py --dataset 'cifar10-8-wen2023adversarial' --model resnet18_cifar --training_step attacked_classification_step  --atk PGD --atk-eps 0.03137254901 --atk-alpha 0.00784313725 --atk-steps 10 --optimizer AdamW --scheduler CosineAnnealingLR  --scheduler-T_max 200 --epochs 201
 
 # srun --partition=researchlong --gres=gpu:1 python cifar10-coerced.py --dataset 'cifar10-16-huang2021unlearnable' --model wideresnet --model-depth 34 --model-widen_factor 10 --model-drop_rate 0.3 --optimizer SGD --optimizer-lr 0.1 --scheduler CosineAnnealingLR  --scheduler-T_max 200 --epochs 201 --training_step classification_step
 
-# srun --partition=researchlong --gres=gpu:1 python cifar10-coerced.py --dataset 'cifar10-16-huang2021unlearnable' --model resnet18_cifar --training_step classification_step --optimizer SGD --optimizer-lr 0.1 --scheduler CosineAnnealingLR  --scheduler-T_max 200 --epochs 201
+# srun --partition=researchlong --gres=gpu:1 python cifar10-coerced.py --dataset 'cifar10-8-huang2021unlearnable' --model resnet18_cifar --training_step classification_step --optimizer SGD --optimizer-lr 0.1 --scheduler CosineAnnealingLR  --scheduler-T_max 200 --epochs 201
 
 
